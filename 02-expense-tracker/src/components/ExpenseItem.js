548df8862722c9a1a98bdb5579cc3a9a02b12260
Serena (PC) <taymeizheng@gmail.com>
2023-04-24 16:58:36 +0800

    WIP till 38

---
+++ 02-expense-tracker/src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css'
+import ExpenseItemDate from './ExpenseItemDate';
+
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <ExpenseItemDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

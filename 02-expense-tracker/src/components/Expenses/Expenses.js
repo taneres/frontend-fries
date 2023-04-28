8a977a64cdf89e31ca908ebde4854a0dfe455ba9
Serena (PC) <taymeizheng@gmail.com>
2023-04-28 18:25:26 +0800

    WIP done with Section 4

--- 02-expense-tracker/src/components/Expenses/Expenses.js
+++ 02-expense-tracker/src/components/Expenses/Expenses.js
@@ -1,20 +1,34 @@
-import ExpenseItem from "./ExpenseItem";
+import { useState } from 'react';
 import Card from '../UI/Card'
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from './ExpensesFilter';
 import "./Expenses.css"
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log("In Expenses.js : Selected " + selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      ;
-    </Card>
+    <div>
+      <Card className="expenses">
+        {/* ExpensesFilter is a "controlled component" as we use 2-way binding
+            a.k.a. parent component fully specifies behaviour. We say the data
+            is driven by props rather than its own local state. */}
+        <ExpensesFilter selectedYear={filteredYear} onFilterYear={filterYearHandler} />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))};
+      </Card>
+    </div>
   );
 }
 
